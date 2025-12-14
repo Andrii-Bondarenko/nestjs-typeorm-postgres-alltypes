5cc561b891dce468e879d04a36f856860362b0c7
Andrii-Bondarenko <andrii_bondarenko2@epam.com>
2025-12-14 10:38:52 +0200

    Adding typeorm and user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../common/base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() user: CreateUserDto): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.usersService.remove(id);
+  }
+}
